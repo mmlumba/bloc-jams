fa7e78245ab2e883eeccd240093fc51ec7441d17
mmlumba <mmlumba@gmail.com>
2015-06-29 19:32:32 -0400

    Changed slider to update music location data on change.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,11 @@ var albumPicasso = {
   albumArtUrl: '/images/album-placeholder.png',
 
   songs: [
-     { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-     { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-     { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-     { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-     { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+     { name: 'Blue', length: 163.38, audioUrl: '/music/placeholders/blue' },
+     { name: 'Green', length: 105.66 , audioUrl: '/music/placeholders/green' },
+     { name: 'Red', length: 270.14, audioUrl: '/music/placeholders/red' },
+     { name: 'Pink', length: 154.81, audioUrl: '/music/placeholders/pink' },
+     { name: 'Magenta', length: 375.92, audioUrl: '/music/placeholders/magenta' }
     ]
 };
 
@@ -34,35 +34,49 @@ blocJams.directive('slider', ['$document', function($document){
       return offsetXPercent;
     }
 
-  /*var updateSeekPercentage = function($seekBar, event) {
-     var barWidth = $seekBar.width();
-     var offsetX =  event.pageX - $seekBar.offset().left;
+   var numberFromValue = function(value, defaultValue) {
+     if (typeof value === 'number') {
+       return value;
+     }
 
-     var offsetXPercent = (offsetX  / $seekBar.width()) * 100;
-     offsetXPercent = Math.max(0, offsetXPercent);
-     offsetXPercent = Math.min(100, offsetXPercent);
+     if(typeof value === 'undefined') {
+       return defaultValue;
+     }
 
-     var percentageString = offsetXPercent + '%';
-     $seekBar.find('.fill').width(percentageString);
-     $seekBar.find('.thumb').css({left: percentageString});
-   }*/
+     if(typeof value === 'string') {
+       return Number(value);
+     }
+   }
 
   return {
     templateUrl: '/templates/directives/slider.html', // We'll create these files shortly.
     replace: true,
     restrict: 'E',
-    scope: {},
+    scope: {
+      onChange: '&'
+    },
     link: function(scope, element, attributes) {
 
        // These values represent the progress into the song/volume bar, and its max value.
        // For now, we're supplying arbitrary initial and max values.
        scope.value = 0;
-       scope.max = 200;
+       scope.max = 100;
 
       var $seekBar = $(element);
 
+      attributes.$observe('value', function(newValue) {
+        scope.value = numberFromValue(newValue, 0);
+      });
+
+      attributes.$observe('max', function(newValue) {
+        scope.max = numberFromValue(newValue, 100) || 100;
+      });
+
+
       var percentString = function () {
-        var percent = Number(scope.value) / Number(scope.max) * 100;
+        var value = scope.value || 0;
+        var max = scope.max || 100;
+        percent = value / max * 100;
         return percent + "%";
       }
 
@@ -77,12 +91,14 @@ blocJams.directive('slider', ['$document', function($document){
       scope.onClickSlider = function(event) {
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.value = percent * scope.max;
+         notifyCallback(scope.value);
        }
       scope.trackThumb = function() {
        $document.bind('mousemove.thumb', function(event){
          var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
          scope.$apply(function(){
            scope.value = percent * scope.max;
+           notifyCallback(scope.value);
          });
        });
 
@@ -92,6 +108,12 @@ blocJams.directive('slider', ['$document', function($document){
          $document.unbind('mouseup.thumb');
        });
      };
+
+     var notifyCallback = function(newValue) {
+         if(typeof scope.onChange === 'function') {
+           scope.onChange({value: newValue});
+         }
+       };
     }
   };
 }]);
@@ -273,6 +295,15 @@ blocJams.controller('Landing.controller', ['$scope', 'ConsoleLogger', function($
        var song = this.currentAlbum.songs[currentTrackIndex];
        this.setSong(this.currentAlbum, song);
      },
+
+     seek: function(time) {
+       // Checks to make sure that a sound file is playing before seeking.
+       if(currentSoundFile) {
+         // Uses a Buzz method to set the time of the song.
+         currentSoundFile.setTime(time);
+       }
+     },
+
      setSong: function(album, song) {
        if (currentSoundFile) {
           currentSoundFile.stop();
